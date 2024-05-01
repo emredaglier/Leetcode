c7b15c44fd8139a1b4f2f7dcf65558c5328742c4
emredaglier <emredagliercsgo@gmail.com>
2024-05-01 11:34:34 +0300

    Test

---
+++ may/2.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# [1, 2, 3]
+# [5, 9 ,3]
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        for index, val in enumerate(max(l1, l2, key=len)):
+            pass
+
+
+
+
+
+        
